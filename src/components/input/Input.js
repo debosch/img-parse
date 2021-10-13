21cac6b052bafeda7e9365443256dc1a948b8450
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2021-10-13 15:34:40 +0600

    Redundant code was cleared

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { ADD_IMAGE } from '../../actions/types';
 
